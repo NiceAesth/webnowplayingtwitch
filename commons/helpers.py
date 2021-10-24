e2c8a4a0692c4f836350b7d648145cf1c9e8659f
Baciu Andrei <baciu0andrei@gmail.com>
2021-10-24 15:09:14 +0300

    Add type hinting

--- commons/helpers.py
+++ commons/helpers.py
@@ -6,8 +6,8 @@ def get_initial_channels(config: Config) -> list[str]:
 
     return config.initial_channels
 
-def list_module(directory):
+def list_module(directory) -> list[str]:
     return (f for f in os.listdir(directory) if f.endswith('.py'))
 
-def split_file_ext(filename):
+def split_file_ext(filename) -> str:
     return os.path.splitext(filename)[0]
